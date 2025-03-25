447608707811d9d17058c54e59033f691ce60ebe
Damon Wang <damon@e-diary.com>
2025-03-25 21:55:13 +1100

    Local test case uplift

--- main_test.go
+++ main_test.go
@@ -36,9 +36,9 @@ type TestSuite struct {
 func setupTestSuite(t *testing.T) *TestSuite {
 	// Setup test database
 	testDB := utils.SetupTestDB(t)
-	t.Cleanup(func() {
-		utils.TeardownTestDB(t, testDB.Client)
-	})
+	// t.Cleanup(func() {
+	// 	utils.TeardownTestDB(t, testDB.Client)
+	// })
 
 	// Initialize repositories
 	moviesRepo := mongodb_repo.NewMovieMongodbRepo(testDB.Config, testDB.Client)
@@ -91,9 +91,11 @@ func setupTestSuite(t *testing.T) *TestSuite {
 }
 
 // HTTP Layer Tests
+// go test -v -run TestMoviesRoutes
 func TestMoviesRoutes(t *testing.T) {
 	suite := setupTestSuite(t)
 
+	// go test -v -run TestMoviesRoutes/GET_/movies
 	t.Run("GET /movies", func(t *testing.T) {
 		w := makeRequest(t, suite.router, "GET", "/movies", nil)
 		assert.Equal(t, http.StatusOK, w.Code)
@@ -101,20 +103,24 @@ func TestMoviesRoutes(t *testing.T) {
 		var response []*models.Movie
 		decodeResponse(t, w, &response)
 		assert.Len(t, response, 2)
+		fmt.Printf("Response: %+v\n", response[0])
 		assert.Equal(t, "Test Movie 1", response[0].Title)
 		assert.Equal(t, "Test Movie 2", response[1].Title)
 	})
 
+	// go test -v -run "TestMoviesRoutes/GET_/movies_with_pagination"
 	t.Run("GET /movies with pagination", func(t *testing.T) {
 		w := makeRequest(t, suite.router, "GET", "/movies?page=1&size=1", nil)
 		assert.Equal(t, http.StatusOK, w.Code)
 
 		var response []*models.Movie
 		decodeResponse(t, w, &response)
+		fmt.Printf("Response: %+v\n", response[0])
 		assert.Len(t, response, 1)
 		assert.Equal(t, "Test Movie 1", response[0].Title)
 	})
 
+	// go test -v -run "TestMoviesRoutes/GET_/movies/:id"
 	t.Run("GET /movies/:id", func(t *testing.T) {
 		movieID := "60c72b2f9b1e8a5d6c8b4567"
 		w := makeRequest(t, suite.router, "GET", "/movies/"+movieID, nil)
@@ -122,23 +128,28 @@ func TestMoviesRoutes(t *testing.T) {
 
 		var response *models.Movie
 		decodeResponse(t, w, &response)
+		fmt.Printf("Response: %+v\n", response)
 		assert.NotNil(t, response)
 		assert.Equal(t, "Test Movie 1", response.Title)
 	})
 }
 
+// go test -v -run TestCommentsRoutes
 func TestCommentsRoutes(t *testing.T) {
 	suite := setupTestSuite(t)
 
+	// go test -v -run "TestCommentsRoutes/GET_/comments"
 	t.Run("GET /comments", func(t *testing.T) {
 		w := makeRequest(t, suite.router, "GET", "/comments", nil)
 		assert.Equal(t, http.StatusOK, w.Code)
 
 		var response []*models.Comment
 		decodeResponse(t, w, &response)
+		fmt.Printf("Response: %+v\n", response[0])
 		assert.Len(t, response, 2)
 	})
 
+	// go test -v -run "TestCommentsRoutes/GET_/comments/:id"
 	t.Run("GET /comments/:id", func(t *testing.T) {
 		commentID := "60c72b2f9b1e8a5d6c8b4567"
 		w := makeRequest(t, suite.router, "GET", "/comments/"+commentID, nil)
@@ -146,10 +157,12 @@ func TestCommentsRoutes(t *testing.T) {
 
 		var response *models.Comment
 		decodeResponse(t, w, &response)
+		fmt.Printf("Response: %+v\n", response)
 		assert.NotNil(t, response)
 		assert.Equal(t, "test1@example.com", response.Email)
 	})
 
+	// go test -v -run "TestCommentsRoutes/POST_/comments"
 	t.Run("POST /comments", func(t *testing.T) {
 		// Create request body with correct field name
 		requestBody := map[string]interface{}{
@@ -173,8 +186,70 @@ func TestCommentsRoutes(t *testing.T) {
 		}
 
 		decodeResponse(t, w, &response)
+		fmt.Printf("Response: %+v\n", response)
 		assert.Equal(t, "Comment created successfully", response.Message)
 		assert.NotEmpty(t, response.Id)
+	})
+
+	// go test -v -run "TestCommentsRoutes/PUT_/comments/:id"
+	t.Run("PUT /comments/:id", func(t *testing.T) {
+		commentID := "60c72b2f9b1e8a5d6c8b4567"
+
+		// Create update request body
+		requestBody := map[string]interface{}{
+			"text": "This is an updated comment text",
+			"name": "Updated Name",
+		}
+
+		// Log the request body
+		body, _ := json.Marshal(requestBody)
+		fmt.Printf("Request body: %s\n", string(body))
+
+		w := makeRequest(t, suite.router, "PUT", "/comments/"+commentID, requestBody)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var response struct {
+			Message string `json:"message"`
+		}
+
+		decodeResponse(t, w, &response)
+		fmt.Printf("Response: %+v\n", response)
+		assert.Equal(t, "Comment updated successfully", response.Message)
+
+		// Verify the update by fetching the comment
+		w = makeRequest(t, suite.router, "GET", "/comments/"+commentID, nil)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var comment *models.Comment
+		decodeResponse(t, w, &comment)
+		fmt.Printf("Updated comment: %+v\n", comment)
+		assert.Equal(t, "This is an updated comment text", comment.Text)
+		assert.Equal(t, "Updated Name", comment.Name)
+	})
+
+	// go test -v -run "TestCommentsRoutes/DELETE_/comments/:id"
+	t.Run("DELETE /comments/:id", func(t *testing.T) {
+		commentID := "60c72b2f9b1e8a5d6c8b4567"
+
+		// First verify the comment exists
+		w := makeRequest(t, suite.router, "GET", "/comments/"+commentID, nil)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		// Delete the comment
+		w = makeRequest(t, suite.router, "DELETE", "/comments/"+commentID, nil)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var response struct {
+			Message string `json:"message"`
+		}
+
+		decodeResponse(t, w, &response)
+		fmt.Printf("Response: %+v\n", response)
+		assert.Equal(t, "Comment deleted successfully", response.Message)
+
+		// Verify the comment is deleted by trying to fetch it
+		w = makeRequest(t, suite.router, "GET", "/comments/"+commentID, nil)
+		assert.Equal(t, http.StatusNotFound, w.Code)
 	})
 }
 
